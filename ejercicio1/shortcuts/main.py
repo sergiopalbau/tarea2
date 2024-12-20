20883149163724fa04fd5643cd30f40a79e86a54
sergiopalbau <s.palomo.b@gmail.com>
2024-12-20 20:10:42 +0100

    pypas match case

--- ejercicio1/shortcuts/main.py
+++ ejercicio1/shortcuts/main.py
@@ -1,5 +1,26 @@
 def run(key1: str, key2: str, key3: str) -> str:
-    # TODO
+    # creamos una lista con las entradas
+    lista = [key1, key2, key3]
+
+    match lista:
+
+        case ["CTRL", "ALT", "T"]:
+            return "Open terminal"
+        case ["CTRL", "ALT", "L"]:
+            return "Lock screen"
+        case ["CTRL", "ALT", "D"]:
+            return "Show desktop"
+        case ["ALT", "F2", ""]:
+            return "Run console"
+        case ["CTRL", "Q", ""]:
+            return "Close window"
+        case ["CTRL", "ALT", "DEL"]:
+            return "Log out"
+        case _:  # valor no encontrado
+            return "Undefined"
+    # para convervar la estructura de control de pypas asignamos undefined 
+    # a su return
+    action = "Undefined"
     return action
 
 
